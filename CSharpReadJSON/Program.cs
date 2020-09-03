a253f5a5a5af922176e4aa01a614310e43fd2ac7
Bilge Adam <bilge.adam@baogrenci.com>
2020-09-03 12:52:35 +0300

    update

--- CSharpReadJSON/Program.cs
+++ CSharpReadJSON/Program.cs
@@ -16,31 +16,58 @@ namespace CSharpReadJSON
         static void Main(string[] args)
         {
 
+            var path = @"C:\Users\bilge.adam\Desktop\CagatayBoost\okuyombenya.txt";
+
+            string[] lines = File.ReadAllLines(path, Encoding.UTF8);
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
 
 
-            using (StreamReader file = File.OpenText(@"C:\Users\bilge.adam\source\repos\CSharpReadJSON\CSharpReadJSON\users.json"))
+
+            using (StreamReader file = File.OpenText(@"C:\Users\bilge.adam\source\repos\CSharpReadJSON\CSharpReadJSON\firms.json"))
             {
                 using (JsonTextReader reader = new JsonTextReader(file))
                 {
                     JObject o2 = (JObject)JToken.ReadFrom(reader);
 
-                    JToken kullanicilar = o2["users"];
+                    JToken firms = o2["firm"];
 
-                    List<User> csharpusers = new List<User>();
-                    foreach (var item in kullanicilar)
+
+                    List<Student> students = new List<Student>();
+
+                    foreach (var item in firms)
                     {
-                        User newuser = new User();
+                        var data = item["students"];
 
-                        newuser.Name = item["name"].ToString();
-                        newuser.City = item["city"].ToString();
-                        newuser.EMail = item["email"].ToString();
-                        newuser.Phone = item["phone"].ToString();
-                        
-                        newuser.RegisterDate = DateTime.ParseExact(item["registerdate"].ToString(), "dd/mm/yy", null);
+                        foreach (var item2 in data)
+                        {
+                            Student student = new Student();
+                            student.Name = item2["name"].ToString();
+                            student.Surname= item2["surname"].ToString();
 
-                        csharpusers.Add(newuser);
-
+                            students.Add(student);
+                        }
                     }
+
+
+                    //List<User> csharpusers = new List<User>();
+                    //foreach (var item in kullanicilar)
+                    //{
+                    //    User newuser = new User();
+
+                    //    newuser.Name = item["name"].ToString();
+                    //    newuser.City = item["city"].ToString();
+                    //    newuser.EMail = item["email"].ToString();
+                    //    newuser.Phone = item["phone"].ToString();
+                        
+                    //    newuser.RegisterDate = DateTime.ParseExact(item["registerdate"].ToString(), "dd/mm/yy", null);
+
+                    //    csharpusers.Add(newuser);
+
+                    //}
                 }
 
             }
